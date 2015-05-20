f7e68640a894c12d8015ef59e03e85e15e2f797c
Erin <erin.mohat@gmail.com>
2015-05-19 20:45:15 -0600

    assignment 24 updated, complete

--- db/migrate/20150512033945_create_comments.rb
+++ db/migrate/20150512033945_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :body
-      t.refereces :post
+      t.references :post
 
       t.timestamps null: false
     end
