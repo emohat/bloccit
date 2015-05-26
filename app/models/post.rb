930ca626bdb46c21e93f5d76617852805ae9ca10
Erin <erin.mohat@gmail.com>
2015-05-26 17:40:20 -0600

    assigment 32 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ActiveRecord::Base
   posts = Post.all
 
   default_scope { order('created_at DESC') }
-  scope :ordered_by_title, -> { posts.order(:title) }
-  scope :ordered_by_reverse_created_at, -> { posts.order('created_at DESC').reverse }
+  scope :ordered_by_title, -> { reorder(:title) }
+  scope :ordered_by_reverse_created_at, -> { reorder('created_at DESC').reverse }
 end
+
