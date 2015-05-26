76bbc39b52b96f4292b3c05b1308f22dedf80be7
Erin <erin.mohat@gmail.com>
2015-05-26 12:49:35 -0600

    assignment 31 ricky checked

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :flash_attack
+  # before_action :flash_attack
  
-    def flash_attack
-      flash[:notice] = "You were just flashed."
-    end
+  #   def flash_attack
+  #     flash[:notice] = "You were just flashed."
+  #   end
 
 
 end
