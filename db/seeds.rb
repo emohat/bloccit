f2545bd3cc1553b55374aa0e5f6ebeae9637bab7
Erin <erin.mohat@gmail.com>
2015-05-29 13:29:58 -0600

    checkpoint 34/40 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,19 +12,19 @@ require 'faker'
  end
  users = User.all
 
-# Note: by calling `User.new` instead of `create`,
-# we create an instance of User which isn't immediately saved to the database.
- 
-# The `skip_confirmation!` method sets the `confirmed_at` attribute
-# to avoid triggering an confirmation email when the User is saved.
- 
-# The `save` method then saves this User to the database.
- 
+15.times do
+  Topic.create!(
+    name:        Faker::Lorem.sentence,
+    description:  Faker::Lorem.paragraph
+    )
+end
+topics = Topic.all
 
  # Create Posts
  50.times do
    Post.create!(
     user:   users.sample,
+    topic:  topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
    )
@@ -40,13 +40,36 @@ require 'faker'
    )
  end
 
- user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-    email:'erin.mohat@gmail.com',
-    password: 'CodeWork21')
+#create an admin user
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+  )
+admin.skip_confirmation!
+admin.save!
+#create a moderator
+moderator = User.new(
+  name:     'Moderator User',
+  email:    'moderator@example.com',
+  password: 'helloworld',
+  role:     'moderator'
+  )
+  moderator.skip_confirmation!
+  moderator.save!
+  #create a member
+  member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+  )
+  member.skip_confirmation!
+  member.save!
+
 
  puts "Seed finished"
  puts "#{User.count} users created."
  puts "#{Post.count} posts created."
  puts "#{Comment.count} comments created."
+ puts "#{Topic.count} topics created."
